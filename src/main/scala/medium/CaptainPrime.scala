e4b39225bde30943ad57e5a8c6431d78239b5b2e
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-22 11:56:30 +0530

    Add files via upload

---
+++ src/main/scala/medium/CaptainPrime.scala
@@ -0,0 +1,67 @@
+package com.hackerrank.assignment.medium
+
+import scala.math.pow
+import scala.util.control.Breaks._
+
+object CaptainPrime {
+  def main(args: Array[String]): Unit = {
+    var list =  List(43, 23, 66,29)   //3137, 1367, 2333, 101, 12
+                                                  // 43, 23, 66,29
+    var isPrime = getPrimeNumbers(3167)
+    for(i<-list){
+      println(getPosition(i,isPrime))
+    }
+  }
+
+  def getPosition(num: Int, isPrime: Array[Boolean]): String = {
+    var numLeft: Int = num
+    var numRight: Int = num
+    var isPositionRight: Boolean = true
+    var isPositionLeft: Boolean = true
+    if(num.toString.contains('0')) "DEAD"
+    else {
+      breakable {
+        while (numRight > 0) {
+          if (!isPrime(numRight)) {
+            isPositionRight = false
+            break
+          }
+          numRight = numRight / 10
+        }
+      }
+      var mod: Int = pow(10, num.toString.length).toInt
+//      println("mod = " + mod)
+      breakable {
+        while (numLeft > 0) {
+          if (!isPrime(numLeft)) {
+            isPositionLeft = false
+            break
+          }
+          numLeft = numLeft % mod
+          mod /= 10
+        }
+      }
+
+      if (isPositionRight && isPositionLeft) "CENTRAL"
+      else if (isPositionRight) "RIGHT"
+      else if (isPositionLeft) "LEFT"
+      else "DEAD"
+    }
+  }
+
+
+  def getPrimeNumbers(num: Int): Array[Boolean] = {
+    var isPrime: Array[Boolean] = Array.fill(num + 1)(true)
+    for (i <- 2 to num; if i * i <= num) {
+      if (isPrime(i)) {
+        var j = 2 * i
+        while (j <= num) {
+          isPrime(j) = false
+          j += i
+        }
+      }
+    }
+    isPrime
+  }
+}
+
---
+++ src/main/scala/medium/SuperDigit.scala
@@ -0,0 +1,18 @@
+package com.hackerrank.assignment.medium
+
+import scala.annotation.tailrec
+
+object SuperDigit {
+  def main(args:Array[String]):Unit ={
+    var n = 148.toString.toCharArray.map(char => char.toInt-'0')
+    var k = 3
+    var digits:IndexedSeq[Int] = for(_ <- 0 until k) yield f(n)
+    println(f(digits.toArray))
+  }
+
+  def f(digits:Array[Int]):Int ={
+//    println(digits.mkString(" "))
+    if(digits.length == 1) digits(0)
+    else f(digits.sum.toString.toCharArray.map(char => char.toInt -'0'))
+  }
+}
