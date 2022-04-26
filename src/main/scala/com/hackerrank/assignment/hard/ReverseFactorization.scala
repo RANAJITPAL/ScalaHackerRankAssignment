b31d8f740bf67db5a236eba4a9e9a7c7ec0b4819
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-26 11:35:41 +0530

    Update ReverseFactorization.scala

--- src/main/scala/com/hackerrank/assignment/hard/ReverseFactorization.scala
+++ src/main/scala/com/hackerrank/assignment/hard/ReverseFactorization.scala
@@ -13,12 +13,15 @@ object ReverseFactorization {
     //    var num: Int = 72
     //    var n: Int = 9
     //    var arr:Array[Int] = Array(2, 4, 6, 9, 3, 7, 16, 10, 5)
-
+    
+    
+    //Initializing the DP array
     var dp: Array[Int] = Array.fill(num + 1)(Int.MaxValue)
     var result: Array[Int] = new Array[Int](num + 1)
 
     dp(1) = 0
 
+    //filling the DP array upto num
     for (i <- 2 to num) {
       var min: Int = Int.MaxValue
       var prev: Int = Int.MaxValue
@@ -40,6 +43,8 @@ object ReverseFactorization {
     //    println(dp.mkString(" "))
     //    println(result.mkString(" "))
 
+    //checking the condition if it is possible to make the factorization or not 
+    //if possible constructing the factors
     if (dp(num) == Int.MaxValue) println(-1)
     else {
       val r = new ListBuffer[Int]
