682b7897327461a25d6e7d62c5c04f97d30cfeac
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-26 11:29:45 +0530

    Update SuperDigit.scala

--- src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
+++ src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
@@ -2,12 +2,15 @@ package com.hackerrank.assignment.medium
 
 object SuperDigit {
   def main(args: Array[String]): Unit = {
+    //making the string of array to array on Int
     var n = 148.toString.toCharArray.map(char => char.toInt - '0')
     var k = 3
     var digits: IndexedSeq[Int] = for (_ <- 0 until k) yield f(n)
     println(f(digits.toArray))
   }
 
+  //doing the som of all digit's of the array 
+  //if the value is one digit the return else making a recursive call to the function
   def f(digits: Array[Int]): Int = {
     //    println(digits.mkString(" "))
     if (digits.length == 1) digits(0)
