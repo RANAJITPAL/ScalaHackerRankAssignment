274607e0868b5ca02e8da1bf2b6d7baaf1134eab
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-26 11:46:46 +0530

    Update StockPrediction.scala

--- src/main/scala/com/hackerrank/assignment/hard/StockPrediction.scala
+++ src/main/scala/com/hackerrank/assignment/hard/StockPrediction.scala
@@ -14,10 +14,13 @@ object StockPrediction {
 
       var num = arr(d)
       var left = d
+      
+      //finding the left elements that are greater or equal num and less or equal num+M
       while (left >= 0 && num <= arr(left) && arr(left) <= num + M) {
         left -= 1
       }
 
+      /finding the right elements that are greater or equal num and less or equal num+M
       var right = d
       while (right < n && num <= arr(right) && arr(right) <= num + M) {
         right += 1
