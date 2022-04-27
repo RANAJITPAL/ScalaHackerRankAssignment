a23318e7a04f9db127cad37b346770a16a717c3a
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-27 12:21:44 +0530

    Update StockPrediction.scala

--- src/main/scala/com/hackerrank/assignment/hard/StockPrediction.scala
+++ src/main/scala/com/hackerrank/assignment/hard/StockPrediction.scala
@@ -3,30 +3,28 @@ package com.hackerrank.assignment.hard
 import java.util.Scanner
 
 object StockPrediction {
-  def main(args: Array[String]): Unit = {
+  def main(args:Array[String]):Unit ={
     var sc = new Scanner(System.in)
-    var n: Int = sc.nextInt()
-    var arr: Array[Int] = Array.fill(n)(sc.nextInt())
-    var q: Int = sc.nextInt()
-    for (i <- 0 until q) {
-      var d: Int = sc.nextInt()
-      var M: Int = sc.nextInt()
+    var n:Int = sc.nextInt()
+    var arr:Array[Int] = Array.fill(n)(sc.nextInt())
+    var q:Int = sc.nextInt()
+    for(i <- 0 until q){
+      var d:Int = sc.nextInt()
+      var M:Int = sc.nextInt()
 
       var num = arr(d)
       var left = d
-      
-      //finding the left elements that are greater or equal num and less or equal num+M
-      while (left >= 0 && num <= arr(left) && arr(left) <= num + M) {
+      while(left >= 0 && num <= arr(left) && arr(left)<= num+M){
         left -= 1
       }
 
-      /finding the right elements that are greater or equal num and less or equal num+M
       var right = d
-      while (right < n && num <= arr(right) && arr(right) <= num + M) {
+      while(right<n && num <= arr(right) && arr(right)<=num+M){
         right += 1
       }
-      println(right - left - 1)
+      println(right-left-1)
     }
 
   }
 }
+
