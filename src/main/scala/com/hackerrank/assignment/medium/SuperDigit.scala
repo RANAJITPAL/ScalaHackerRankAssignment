125988a7eaad6abcb78cf1d9e9c4781649ce44a1
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-27 11:51:32 +0530

    Update SuperDigit.scala

--- src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
+++ src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
@@ -1,19 +1,21 @@
 package com.hackerrank.assignment.medium
 
+import scala.io.StdIn.readLine
+import scala.annotation.tailrec
+
 object SuperDigit {
-  def main(args: Array[String]): Unit = {
-    //making the string of array to array on Int
-    var n = 148.toString.toCharArray.map(char => char.toInt - '0')
-    var k = 3
-    var digits: IndexedSeq[Int] = for (_ <- 0 until k) yield f(n)
+ def main(args:Array[String]):Unit ={
+   var input = readLine().split(" ")
+    var n = input(0).toString.toCharArray.map(char => char.toInt-'0')
+    var k = input(1).toInt
+    var digits:IndexedSeq[Int] = for(_ <- 0 until k) yield f(n)
     println(f(digits.toArray))
   }
 
-  //doing the som of all digit's of the array 
-  //if the value is one digit the return else making a recursive call to the function
-  def f(digits: Array[Int]): Int = {
-    //    println(digits.mkString(" "))
-    if (digits.length == 1) digits(0)
-    else f(digits.sum.toString.toCharArray.map(char => char.toInt - '0'))
+    @tailrec 
+  def f(digits:Array[Int]):Int ={
+//    println(digits.mkString(" "))
+    if(digits.length == 1) digits(0)
+    else f(digits.sum.toString.toCharArray.map(char => char.toInt -'0'))
   }
 }
