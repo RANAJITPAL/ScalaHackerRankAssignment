ff31fbce12fe53ee3b936ba0d94c14d9249d264e
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-27 10:34:16 +0530

    Update StringCompression.scala

--- src/main/scala/com/hackerrank/assignment/easy/StringCompression.scala
+++ src/main/scala/com/hackerrank/assignment/easy/StringCompression.scala
@@ -1,40 +1,45 @@
 package com.hackerrank.assignment.easy
 
 import scala.collection.mutable.ListBuffer
+import scala.io.StdIn.readLine
 
 object StringCompression {
-  def main(args: Array[String]): Unit = {
-    println(f("aaab"))
+  def main(args:Array[String]):Unit ={
+//    println(f("aaabbbcc")) //a3b3c2
+    var input = readLine();
+    println(f(input))
   }
 
-  def f(str: String): String = {
-    var count: Int = 1
-    var lastChar: Char = str.charAt(0)
-    var result: ListBuffer[String] = ListBuffer()
-    for (i <- 1 until str.length - 1) {
-      if (str.charAt(i) != lastChar) {
-        if (count == 1) {
+  def f(str:String): String ={
+  if(str.length == 1) return str
+    var count:Int = 1
+    var lastChar:Char = str.charAt(0)
+    var result:ListBuffer[String] = ListBuffer()
+    for(i <- 1 until str.length-1){
+      if(str.charAt(i) != lastChar){
+        if(count == 1){
           result += lastChar.toString
           lastChar = str.charAt(i)
           count = 1
-        } else {
+        }else{
           result += lastChar.toString + count.toString
           lastChar = str.charAt(i);
           count = 1
         }
-      } else {
+      }else{
         count += 1
       }
     }
-    if (str.charAt(str.length - 1) != lastChar) {
-      if (count == 1) {
-        result += lastChar.toString + str.charAt(str.length - 1).toString
-      } else {
-        result += lastChar.toString + count.toString + str.charAt(str.length - 1).toString
+    if(str.charAt(str.length-1) != lastChar){
+      if(count == 1){
+        result += lastChar.toString+str.charAt(str.length-1).toString
+      }else{
+        result += lastChar.toString + count.toString + str.charAt(str.length-1).toString
       }
-    } else {
-      result += lastChar.toString + (count + 1).toString
+    }else{
+      result += lastChar.toString+(count+1).toString
     }
     result.mkString("")
   }
 }
+
