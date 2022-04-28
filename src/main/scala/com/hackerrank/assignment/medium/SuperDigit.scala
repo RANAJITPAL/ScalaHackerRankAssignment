81d0f068cacdcdd1d3ef64ad225c6ad79ef68725
Ranajit Pal <36149344+RANAJITPAL@users.noreply.github.com>
2022-04-28 13:04:33 +0530

    Update SuperDigit.scala

--- src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
+++ src/main/scala/com/hackerrank/assignment/medium/SuperDigit.scala
@@ -4,12 +4,13 @@ import scala.io.StdIn.readLine
 import scala.annotation.tailrec
 
 object SuperDigit {
- def main(args:Array[String]):Unit ={
+  def main(args:Array[String]):Unit ={
    var input = readLine().split(" ")
     var n = input(0).toString.toCharArray.map(char => char.toInt-'0')
     var k = input(1).toInt
-    var digits:IndexedSeq[Int] = for(_ <- 0 until k) yield f(n)
-    println(f(digits.toArray))
+    //println(f(n))
+    var digits:Array[Int] = (f(n)*k).toString.toCharArray.map(char => char.toInt-'0')
+    println(f(digits))
   }
 
     @tailrec 
